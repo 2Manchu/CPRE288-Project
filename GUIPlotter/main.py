ee9718cb7adcb9f7b9690491ce29d8cc09d07312
2Manchu <42014390+2Manchu@users.noreply.github.com>
2022-04-13 19:12:42 -0500

    Start thinking about a GUI
    Thinking about doing a GUI in Python that plots the object data as a radial graph

---
+++ GUIPlotter/main.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+while 1:
+    with open('data.csv', 'r') as dataFile:
+        Line_reader = csv.reader(dataFile, ',')
+
+        theta = []
+        radius = []
+        size = []
+
+        for
+
+        sleep(1)
